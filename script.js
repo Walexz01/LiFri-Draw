894156c3dd74ce46d4d163a8ed1a00ee3d58e193
Walexz01 <adegbiteadewaleoluwaseun@gmail.com>
2025-05-30 02:22:13 +0100

    complete illustration and reel section

--- script.js
+++ script.js
@@ -68,3 +68,106 @@ prevBtn.addEventListener("click", function () {
   }
   showImage(currentImage);
 });
+
+// reeel video
+
+var videoPlayButton,
+  videoWrapper = document.getElementsByClassName("video-wrapper")[0],
+  video = document.getElementsByTagName("video")[0],
+  videoMethods = {
+    renderVideoPlayButton: function () {
+      if (videoWrapper.contains(video)) {
+        this.formatVideoPlayButton();
+        video.classList.add("has-media-controls-hidden");
+        videoPlayButton = document.getElementsByClassName(
+          "video-overlay-play-button"
+        )[0];
+        videoPlayButton.addEventListener("click", this.hideVideoPlayButton);
+      }
+    },
+
+    formatVideoPlayButton: function () {
+      videoWrapper.insertAdjacentHTML(
+        "beforeend",
+        '\
+                <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
+                    <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>\
+                    <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
+                </svg>\
+            '
+      );
+    },
+
+    hideVideoPlayButton: function () {
+      video.play();
+      videoPlayButton.classList.add("is-hidden");
+      video.classList.remove("has-media-controls-hidden");
+      video.setAttribute("controls", "controls");
+    },
+  };
+
+videoMethods.renderVideoPlayButton();
+
+// illustration
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slideWrap = document.querySelector(".slide_wrap");
+  const slideShow = slideWrap.querySelector(".slide_show");
+  const slideImg = slideShow.querySelector(".slide_img");
+  const prevBtn = slideWrap.querySelector(".prev");
+  const nextBtn = slideWrap.querySelector(".next");
+
+  const illustrationImg = [
+    "assets/Ilustraciones_image1.png",
+    "assets/Ilustraciones_image2.png",
+    "assets/Ilustraciones_image3.png",
+    "assets/Ilustraciones_image4.png",
+    "assets/Ilustraciones_image5.png",
+    "assets/Ilustraciones_image6.png",
+    "assets/Ilustraciones_image7.png",
+    "assets/Ilustraciones_image8.png",
+    "assets/Ilustraciones_image9.png",
+    "assets/Ilustraciones_image10.jpg",
+    "assets/Ilustraciones_image11.png",
+    "assets/Ilustraciones_image12.png",
+    "assets/Ilustraciones_image13.png",
+  ];
+
+  illustrationImg.forEach((src) => {
+    const div = document.createElement("div");
+    const image = document.createElement("img");
+    div.classList.add("slide");
+    image.src = src;
+    div.appendChild(image);
+    slideImg.appendChild(div);
+  });
+
+  const slides = slideImg.querySelectorAll(".slide");
+  const slideCount = slides.length;
+  const slideWidth = slides[0].offsetWidth;
+  const showNum = 3; // number of visible slides
+  let currentIndex = 0;
+
+  function slideTo(index) {
+    slideImg.style.transition = "margin-left 0.5s";
+    slideImg.style.marginLeft = -(slideWidth * index) + "px";
+  }
+
+  function goNext() {
+    // Stop when last visible group is shown
+    if (currentIndex < slideCount - showNum) {
+      currentIndex++;
+      slideTo(currentIndex);
+    }
+  }
+
+  function goBack() {
+    if (currentIndex > 0) {
+      currentIndex--;
+      slideTo(currentIndex);
+    }
+  }
+
+  nextBtn.addEventListener("click", goNext);
+  prevBtn.addEventListener("click", goBack);
+});
